6d954efab30c1d4bf6b8b93e65927cfa735e8fa4
sebjmz <147550659+sebjmz@users.noreply.github.com>
2026-06-26 22:15:50 -0400

    Delete paypal directory

--- paypal/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("PAYPAL_CLIENT_ID","AQUI_TU_CLIENT_ID");
-define("PAYPAL_SECRET","AQUI_TU_SECRET");
-
-define("PAYPAL_BASE","https://api-m.paypal.com");
-
-// Sandbox:
-// https://api-m.sandbox.paypal.com
